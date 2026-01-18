a24dcb0bf1cf4bd9f03c19742f3624e7dd591f64
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2026-01-18 20:57:15 +0700

    feat: start command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
+mod constants;
+mod handlers;
+mod core;
+mod entities;
+mod utils;
 
+use clap::Parser;
+
+use handlers::repository_handler;
+use utils::message_builder;
+use entities::cli::{
+    Cli,
+    Commands
+};
+
+fn main() {
+    match Cli::try_parse() {
+        Ok(cli) => {
+            match cli.command {
+                Some(Commands::Start) => repository_handler::start(),
+                None => message_builder::missing_commands()
+            }
+        }
+        Err(_) => {
+            message_builder::invalid_commands()
+        }
+    }
 }
