acc0797adcc6ec1d20542ad0d330be858484b947
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2026-01-18 15:37:42 +0700

    v2 initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-mod commands;
-mod repository;
-mod object;
-pub mod constant;
-mod status;
-
-use crate::constant::message::USAGE_MESSAGE;
-
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
 
-    if args.len() < 2 {
-        eprintln!("{}", USAGE_MESSAGE);
-        return;
-    }
-
-    match args[1].as_str() {
-        "init" => commands::init(),
-        "add" => commands::add(&args[2..]),
-        "status" => commands::status(),
-        _ => eprintln!("{}", USAGE_MESSAGE)
-    }
 }
